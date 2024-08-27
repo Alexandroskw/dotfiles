286c4b0def6eb9380e5b18ff8dd227586e7bd6c3
Alexandroskw <filipbilly.120@gmail.com>
2024-08-26 20:02:48 -0600

    Made some changes in the ```alpha.lua``` menu

--- .config/nvim/lua/plugin_config/alpha.lua
+++ .config/nvim/lua/plugin_config/alpha.lua
@@ -15,12 +15,12 @@ dashboard.section.header.val = {
         "     \\__\\/         \\__\\/         \\__\\/           ~~~~                     \\__\\/    ", }
 
 dashboard.section.buttons.val = {
-        dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
-        dashboard.button("Ctrl-f", "？ > Find file", "<cmd>Telescope find_files<CR>"),
+        dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+        dashboard.button("Ctrl-f", "󰈞  > Find file", "<cmd>Telescope find_files<CR>"),
         dashboard.button("SPC ps", "⁋  > Find word", ":Telescope live_grep<CR>"),
-        dashboard.button("Ctrl-e", "↗  > Harpooned files", ":Telescope live_grep<CR>"),
-        dashboard.button("SPC n", "☰  > Open/Close File Tree", ":NvimTreeToggle<CR>"),
-        dashboard.button("q", "⊗  > Quit NVIM", ":qa<CR>"),
+        dashboard.button("Ctrl-p", "  > Find git files", ":Telescope git_files<CR>"),
+        dashboard.button("SPC n", "󰍜  > Open/Close File Tree", ":NvimTreeToggle<CR>"),
+        dashboard.button("q", "󱎘  > Quit NVIM", ":qa<CR>"),
 }
 
 alpha.setup(dashboard.opts)
