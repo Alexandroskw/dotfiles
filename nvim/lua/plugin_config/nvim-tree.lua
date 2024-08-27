c817f9da6a176c600645e5edbc65d19fe3cf049c
Alexandroskw <filipbilly.120@gmail.com>
2024-08-26 19:48:30 -0600

    Removing the folders of the root of the repository

--- nvim/lua/plugin_config/nvim-tree.lua
+++
@@ -1,7 +0,0 @@
-vim.g.loaded_newtrw = 1
-vim.g.loaded_newtrwPlugin = 1
-
-require("nvim-tree").setup()
-
-vim.keymap.set("n", "<leader>n", ":NvimTreeToggle<CR>")
-vim.keymap.set("n", "<leader>nn", ":NvimTreeFindFile<CR>")
