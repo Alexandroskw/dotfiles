d38d48c3c71395e09ce041c555d01ad036e5ee06
Alexandroskw <filipbilly.120@gmail.com>
2025-05-10 20:28:06 -0600

    fix: Adding the description for the keymaps

--- nvim/.config/nvim/lua/plugin_config/fugitive.lua
+++ nvim/.config/nvim/lua/plugin_config/fugitive.lua
@@ -1,2 +1,2 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-vim.keymap.set("n", "<leader>gd", ":Git diff %<CR>")
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Git status" })
+vim.keymap.set("n", "<leader>gd", ":Git diff %<CR>", { desc = "Git diff actual file" })
--- nvim/.config/nvim/lua/plugin_config/harpoon.lua
+++ nvim/.config/nvim/lua/plugin_config/harpoon.lua
@@ -4,10 +4,10 @@ local harpoon = require("harpoon")
 harpoon:setup()
 -- REQUIRED
 
-vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Adding current file to the harpoon list" })
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Display the harpoon list" })
 
-vim.keymap.set("n", "<C-y>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<C-y>", function() harpoon:list():select(1) end, { desc = "Change to the fisrt file" })
+vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end, { desc = "Change to the second file" })
+vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end, { desc = "Change to the third file" })
+vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end, { desc = "Change to the fourth file" })
