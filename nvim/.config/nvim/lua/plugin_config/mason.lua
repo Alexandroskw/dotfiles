cec350ca3fe628d6f6a62b25bb184ac04683b281
Alexandroskw <filipbilly.120@gmail.com>
2025-05-10 20:29:49 -0600

    fix: change the configs for ensured installed lsp with mason

--- nvim/.config/nvim/lua/plugin_config/mason.lua
+++ nvim/.config/nvim/lua/plugin_config/mason.lua
@@ -1,12 +1,26 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-        ensure_installed = {
-                "lua_ls",
-                "pyright",
-                "clangd",
-        },
+	ensure_installed = {
+		"lua_ls",
+		"pyright",
+		"clangd",
+	},
 })
 
-require("lspconfig").lua_ls.setup({})
--- require("lspconfig").rust_analyzer.setup{}
-require("lspconfig").pyright.setup({})
+handlers = {
+	["lua_ls"] = function()
+		require("lspconfig")[lua_ls].setup({})
+	end,
+	["pyright"] = function()
+		require("lspconfig")[pyright].setup({
+			on_attach = on_attach,
+			capabilities = capabilities,
+			filetypes = { "python" },
+		})
+	end,
+	["clangd"] = function()
+		require("lspconfig").clangd.setup({})
+	end,
+}
+
+vim.keymap.set("n", "<leader>m", ":Mason<CR>", { desc = "Install new package with Mason" })
